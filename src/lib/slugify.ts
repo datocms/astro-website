d21f3b90ca6eda0a877569ec4204c8a231fd1c89
Stefano Verna <s.verna@datocms.com>
2024-09-10 17:56:08 +0200

    Navbar

--- src/lib/slugify.ts
+++ src/lib/slugify.ts
@@ -3,10 +3,16 @@
  * lowercase, eliminating non-alphanumeric characters, and removing any hyphens
  * at the beginning or end of the string.
  */
-export const slugify = (str: string | null) =>
-  str
+
+export function slugify(str: string): string;
+export function slugify(str: null | undefined): undefined;
+export function slugify(str: string | null | undefined): string | undefined;
+
+export function slugify(str: string | null | undefined): string | undefined {
+  return str
     ? str
         .toLowerCase()
         .replace(/[^a-z0-9]+/g, '-')
         .replace(/(^-|-$)+/g, '')
     : undefined;
+}
