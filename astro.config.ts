2031430825e2a50353d1a02760c5e6413b1d6aec
Stefano Verna <s.verna@datocms.com>
2024-09-22 12:34:10 +0200

    Better progress bar

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,6 @@ import { defineConfig, envField } from 'astro/config';
 
 import bundlesize from 'vite-plugin-bundlesize';
 
-import vtbot from 'astro-vtbot';
-
 export default defineConfig({
   output: 'server',
   image: {
@@ -46,7 +44,7 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [react(), expressiveCode(), vtbot()],
+  integrations: [react(), expressiveCode()],
   // This project does not use static markdown, only remote. See our <Markdown /> component
   markdown: {},
   vite: {
