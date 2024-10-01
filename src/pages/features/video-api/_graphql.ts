85fee819dcc57ff2e8ac9c3a8a7d7283b3399e76
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-10-01 12:36:10 +0200

    Add video-api skeleton

---
+++ src/pages/features/video-api/_graphql.ts
@@ -0,0 +1,15 @@
+import { TagFragment } from '~/lib/datocms/commonFragments';
+import { graphql } from '~/lib/datocms/graphql';
+
+export const query = graphql(
+  /* GraphQL */ `
+    query VideoApi($slug: String!) {
+      page: feature(filter: { slug: { eq: $slug } }) {
+        _seoMetaTags {
+          ...TagFragment
+        }
+      }
+    }
+  `,
+  [TagFragment],
+);
