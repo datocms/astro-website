893edaf161f00fc74edc6877b5b184b8500d8e5d
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-10-11 15:59:53 +0200

    –

--- src/lib/datocms/gqlUrlBuilder/index.ts
+++ src/lib/datocms/gqlUrlBuilder/index.ts
@@ -1,74 +1,74 @@
 import { type FragmentOf } from '../graphql';
+import { buildUrlForBlogPost, type BlogPostUrlFragment } from './blogPost';
+import { buildUrlForChangelogEntry, type ChangelogEntryUrlFragment } from './changelogEntry';
 import { buildUrlForDocGroup, type DocGroupUrlFragment } from './docGroup';
 import { buildUrlForDocPage, type DocPageUrlFragment } from './docPage';
+import { buildUrlForEnterpriseApp, type EnterpriseAppUrlFragment } from './enterpriseApp';
 import { buildUrlForFeature, type FeatureUrlFragment } from './feature';
+import { buildUrlForHostingApp, type HostingAppUrlFragment } from './hostingApp';
+import { buildUrlForPlugin, type PluginUrlFragment } from './plugin';
 import { buildUrlForProductUpdate, ProductUpdateUrlFragment } from './productUpdate';
 import { buildUrlForTemplateDemo, type TemplateDemoUrlFragment } from './templateDemo';
 import { buildUrlForUserGuideEpisode, type UserGuideEpisodeUrlFragment } from './userGuideEpisode';
-import { buildUrlForBlogPost, type BlogPostUrlFragment } from './blogPost';
-import { buildUrlForChangelogEntry, type ChangelogEntryUrlFragment } from './changelogEntry';
-import { buildUrlForPlugin, type PluginUrlFragment } from './plugin';
-import { buildUrlForHostingApp, type HostingAppUrlFragment } from './hostingApp';
-import { buildUrlForEnterpriseApp, type EnterpriseAppUrlFragment } from './enterpriseApp';
 
 export function buildUrlFromGql(
   thing:
-    | (FragmentOf<typeof DocPageUrlFragment> & {
-        __typename: 'DocPageRecord';
-      })
-    | (FragmentOf<typeof FeatureUrlFragment> & {
-        __typename: 'FeatureRecord';
-      })
-    | (FragmentOf<typeof DocGroupUrlFragment> & {
-        __typename: 'DocGroupRecord';
-      })
-    | (FragmentOf<typeof UserGuideEpisodeUrlFragment> & {
-        __typename: 'UserGuidesVideoRecord';
-      })
-    | (FragmentOf<typeof TemplateDemoUrlFragment> & {
-        __typename: 'TemplateDemoRecord';
-      })
-    | (FragmentOf<typeof ProductUpdateUrlFragment> & {
-        __typename: 'ProductUpdateRecord';
-      })
     | (FragmentOf<typeof BlogPostUrlFragment> & {
         __typename: 'BlogPostRecord';
       })
     | (FragmentOf<typeof ChangelogEntryUrlFragment> & {
         __typename: 'ChangelogEntryRecord';
       })
-    | (FragmentOf<typeof PluginUrlFragment> & {
-        __typename: 'PluginRecord';
+    | (FragmentOf<typeof DocGroupUrlFragment> & {
+        __typename: 'DocGroupRecord';
+      })
+    | (FragmentOf<typeof DocPageUrlFragment> & {
+        __typename: 'DocPageRecord';
+      })
+    | (FragmentOf<typeof EnterpriseAppUrlFragment> & {
+        __typename: 'EnterpriseAppRecord';
+      })
+    | (FragmentOf<typeof FeatureUrlFragment> & {
+        __typename: 'FeatureRecord';
       })
     | (FragmentOf<typeof HostingAppUrlFragment> & {
         __typename: 'HostingAppRecord';
       })
-    | (FragmentOf<typeof EnterpriseAppUrlFragment> & {
-        __typename: 'EnterpriseAppRecord';
+    | (FragmentOf<typeof PluginUrlFragment> & {
+        __typename: 'PluginRecord';
+      })
+    | (FragmentOf<typeof ProductUpdateUrlFragment> & {
+        __typename: 'ProductUpdateRecord';
+      })
+    | (FragmentOf<typeof TemplateDemoUrlFragment> & {
+        __typename: 'TemplateDemoRecord';
+      })
+    | (FragmentOf<typeof UserGuideEpisodeUrlFragment> & {
+        __typename: 'UserGuidesVideoRecord';
       }),
 ) {
   switch (thing.__typename) {
-    case 'DocPageRecord':
-      return buildUrlForDocPage(thing);
-    case 'FeatureRecord':
-      return buildUrlForFeature(thing);
-    case 'DocGroupRecord':
-      return buildUrlForDocGroup(thing);
-    case 'UserGuidesVideoRecord':
-      return buildUrlForUserGuideEpisode(thing);
-    case 'TemplateDemoRecord':
-      return buildUrlForTemplateDemo(thing);
-    case 'ProductUpdateRecord':
-      return buildUrlForProductUpdate(thing);
     case 'BlogPostRecord':
       return buildUrlForBlogPost(thing);
     case 'ChangelogEntryRecord':
       return buildUrlForChangelogEntry(thing);
-    case 'PluginRecord':
-      return buildUrlForPlugin(thing);
-    case 'HostingAppRecord':
-      return buildUrlForHostingApp(thing);
+    case 'DocGroupRecord':
+      return buildUrlForDocGroup(thing);
+    case 'DocPageRecord':
+      return buildUrlForDocPage(thing);
     case 'EnterpriseAppRecord':
       return buildUrlForEnterpriseApp(thing);
+    case 'FeatureRecord':
+      return buildUrlForFeature(thing);
+    case 'HostingAppRecord':
+      return buildUrlForHostingApp(thing);
+    case 'PluginRecord':
+      return buildUrlForPlugin(thing);
+    case 'ProductUpdateRecord':
+      return buildUrlForProductUpdate(thing);
+    case 'TemplateDemoRecord':
+      return buildUrlForTemplateDemo(thing);
+    case 'UserGuidesVideoRecord':
+      return buildUrlForUserGuideEpisode(thing);
   }
 }
