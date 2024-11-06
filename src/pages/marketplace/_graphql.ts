f790cff70500566244b20c81b59123e9eb6882d1
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-11-06 01:08:23 +0100

    Wip

---
+++ src/pages/marketplace/_graphql.ts
@@ -0,0 +1,84 @@
+import { ResponsiveImageFragment } from '~/components/ResponsiveImage/graphql';
+import { PluginUrlFragment } from '~/lib/datocms/gqlUrlBuilder/plugin';
+import { HostingAppUrlFragment } from '~/lib/datocms/gqlUrlBuilder/hostingApp';
+import { EnterpriseAppUrlFragment } from '~/lib/datocms/gqlUrlBuilder/enterpriseApp';
+import { graphql } from '~/lib/datocms/graphql';
+
+export const query = graphql(
+  /* GraphQL */ `
+    query Marketplace {
+      demos: _allTemplateDemosMeta {
+        count
+      }
+      plugins: _allPluginsMeta(filter: { manuallyDeprecated: { eq: false } }) {
+        count
+      }
+      hostingApps: _allHostingAppsMeta {
+        count
+      }
+      enterpriseApps: _allEnterpriseAppsMeta {
+        count
+      }
+      page: integrationsPage {
+        demos {
+          id
+          code
+          name
+          cmsDescription
+          starterType
+          badge {
+            name
+            emoji
+          }
+          label
+          githubRepo
+          technology {
+            name
+            logo {
+              url
+            }
+            squareLogo {
+              url
+            }
+          }
+          screenshot {
+            responsiveImage(imgixParams: { auto: format, w: 600, h: 400, fit: crop }) {
+              ...ResponsiveImageFragment
+            }
+          }
+        }
+        plugins {
+          ...PluginUrlFragment
+          coverImage {
+            responsiveImage(imgixParams: { auto: format, w: 360, h: 240, fit: crop }) {
+              ...ResponsiveImageFragment
+            }
+          }
+          title
+          description
+        }
+        hostingBuilding {
+          ...HostingAppUrlFragment
+          title
+          description: shortDescription
+          logo {
+            url
+            width
+            height
+          }
+        }
+        enterpriseApps {
+          ...EnterpriseAppUrlFragment
+          title
+          description: shortDescription
+          logo {
+            url
+            width
+            height
+          }
+        }
+      }
+    }
+  `,
+  [ResponsiveImageFragment, PluginUrlFragment, HostingAppUrlFragment, EnterpriseAppUrlFragment],
+);
