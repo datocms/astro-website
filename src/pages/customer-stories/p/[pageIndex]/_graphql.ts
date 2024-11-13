b00df6269ea8344c053f5dcbfe8c717bd5a8e56b
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-11-13 21:01:24 +0100

    Customer stories

---
+++ src/pages/customer-stories/p/[pageIndex]/_graphql.ts
@@ -0,0 +1,70 @@
+import { range } from 'lodash-es';
+import { ResponsiveImageFragment } from '~/components/ResponsiveImage/graphql';
+import { TagFragment } from '~/lib/datocms/commonFragments';
+import { executeQueryOutsideAstro } from '~/lib/datocms/executeQuery';
+import { CustomerStoryUrlFragment } from '~/lib/datocms/gqlUrlBuilder/customerStory';
+import { graphql } from '~/lib/datocms/graphql';
+import type { BuildSitemapUrlsFn } from '~/pages/sitemap.xml';
+
+export const perPage = 12;
+
+export const query = graphql(
+  /* GraphQL */ `
+    query CustomerStories($limit: IntType!, $offset: IntType!) {
+      page: customerStoriesIndex {
+        seo: _seoMetaTags {
+          ...TagFragment
+        }
+      }
+
+      posts: allCustomerStories(
+        first: $limit
+        skip: $offset
+        orderBy: [_firstPublishedAt_DESC, _createdAt_DESC]
+      ) {
+        ...CustomerStoryUrlFragment
+        title
+        excerpt {
+          value
+        }
+        coverImage {
+          responsiveImage(imgixParams: { auto: format, w: 800, h: 450, fill: blur }) {
+            ...ResponsiveImageFragment
+          }
+        }
+        people {
+          name
+          title
+          company
+          avatar {
+            responsiveImage(imgixParams: { auto: format, w: 50, h: 50, fit: crop, crop: faces }) {
+              ...ResponsiveImageFragment
+            }
+          }
+        }
+      }
+
+      _allCustomerStoriesMeta {
+        count
+      }
+    }
+  `,
+  [TagFragment, ResponsiveImageFragment, CustomerStoryUrlFragment],
+);
+
+export const buildSitemapUrls: BuildSitemapUrlsFn = async ({ includeDrafts }) => {
+  const {
+    meta: { count },
+  } = await executeQueryOutsideAstro(
+    graphql(/* GraphQL */ `
+      query BuildSitemapUrls {
+        meta: _allCustomerStoriesMeta {
+          count
+        }
+      }
+    `),
+    { includeDrafts },
+  );
+
+  return range(2, 1 + Math.ceil(count / perPage)).map((i) => `/customer-stories/p/${i}`);
+};
