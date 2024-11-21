a63c63174edadc162fd362b7345ae71ac91f55e2
Stefano Verna <s.verna@datocms.com>
2024-11-21 16:09:14 +0100

    Automatic recordToWebsiteRoute

--- src/lib/datocms/recordInfo.ts
+++ src/lib/datocms/recordInfo.ts
@@ -1,15 +1,15 @@
+import { executeQuery } from '@datocms/cda-client';
 import type { Client, SchemaTypes } from '@datocms/cma-client';
+import { DATOCMS_API_TOKEN } from 'astro:env/server';
+import type { TadaDocumentNode } from 'gql.tada';
+import type { FragmentDefinitionNode } from 'graphql';
+import { camelCase, upperFirst } from 'lodash-es';
+import { buildUrlFromGql } from '~/lib/datocms/gqlUrlBuilder';
+import { graphql } from './graphql';
 
-/*
- * Both the "Web Previews" and "SEO/Readability Analysis" plugins from DatoCMS
- * need to know the URL of the site that corresponds to each DatoCMS record to
- * work properly. These two functions are responsible for returning this
- * information, and are utilized by the API routes associated with the two
- * plugins:
- *
- * - server/api/seo-analysis/index.ts
- * - server/api/preview-links/index.ts
- */
+const allUrlBuilderFragments = import.meta.glob('./gqlUrlBuilder/*.ts', {
+  eager: false,
+});
 
 type RecordToWebsiteRouteOptions = {
   item: SchemaTypes.Item;
@@ -20,55 +20,45 @@ type RecordToWebsiteRouteOptions = {
 export async function recordToWebsiteRoute({
   item,
   itemTypeApiKey,
-  client,
 }: RecordToWebsiteRouteOptions): Promise<string | null> {
-  switch (itemTypeApiKey) {
-    case 'page': {
-      return '/';
-    }
-    case 'article': {
-      return `/blog/${await recordToSlug(item, itemTypeApiKey)}`;
-    }
-    case 'changelog': {
-      return `/product-updates`;
-    }
-    case 'changelog_entry': {
-      return `/product-updates/${await recordToSlug(item, itemTypeApiKey)}`;
-    }
-    case 'features_index': {
-      return `/features`;
-    }
-    case 'feature': {
-      return `/features/${await recordToSlug(item, itemTypeApiKey)}`;
-    }
-    case 'partner_home': {
-      return `/partners`;
-    }
-    case 'partner': {
-      return `/partners/${await recordToSlug(item, itemTypeApiKey)}`;
-    }
-    case 'success_stories_index': {
-      return `/customers`;
-    }
-    case 'success_story': {
-      return `/customers/${await recordToSlug(item, itemTypeApiKey)}`;
-    }
-    case 'user_guides_chapter': {
-      return '/user-guides';
-    }
-    case 'user_guides_home': {
-      return '/user-guides';
-    }
-    case 'user_guides_video': {
-      const chapters = await client.items.list({
-        version: 'current',
-        filter: { type: 'user_guides_chapter', fields: { videos: { all_in: [item.id] } } },
-      });
-      return `/user-guides/${chapters[0]!.slug}/${await recordToSlug(item, itemTypeApiKey)}`;
-    }
-    default:
-      return null;
+  const gqlFieldName = camelCase(itemTypeApiKey);
+  const importPath = `./gqlUrlBuilder/${gqlFieldName}.ts`;
+  const modulePromise = allUrlBuilderFragments[importPath];
+
+  if (!modulePromise) {
+    return null;
   }
+
+  const importedModule = (await modulePromise()) as any;
+
+  const fragmentExportName = `${upperFirst(gqlFieldName)}UrlFragment`;
+
+  if (!(fragmentExportName in importedModule)) {
+    throw new Error(`Missing ${fragmentExportName} in ${importPath}!`);
+  }
+
+  const fragment = importedModule[fragmentExportName] as TadaDocumentNode;
+  const fragmentName = (fragment.definitions[0] as FragmentDefinitionNode).name.value;
+
+  const query = graphql(
+    /* GraphQL */ `
+    query UrlBuilder($id: ItemId!) {
+      thing: ${gqlFieldName}(filter: { id: { eq: $id } }) {
+        __typename
+        ...${fragmentName}
+      }
+    }
+  `,
+    [fragment as any],
+  );
+
+  const { thing } = await executeQuery<any, any>(query, {
+    token: DATOCMS_API_TOKEN,
+    includeDrafts: true,
+    variables: { id: item.id },
+  });
+
+  return buildUrlFromGql(thing);
 }
 
 export async function recordToSlug(
