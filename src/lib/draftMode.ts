87e1ce2b9e148a4353ebf36d17911f8dd29048d2
Stefano Verna <s.verna@datocms.com>
2024-11-24 11:52:00 +0100

    Debug

--- src/lib/draftMode.ts
+++ src/lib/draftMode.ts
@@ -6,5 +6,14 @@ export const productionHostname = 'www2.datocms.com';
 export function isDraftModeEnabled(requestOrAstro: Request | AstroGlobal) {
   const url =
     typeof requestOrAstro.url === 'string' ? new URL(requestOrAstro.url) : requestOrAstro.url;
+
+  console.log(
+    url,
+    url.hostname,
+    Object.fromEntries(
+      'headers' in requestOrAstro ? requestOrAstro.headers : requestOrAstro.request.headers,
+    ),
+  );
+
   return url.hostname !== productionHostname;
 }
