421d21e1db54b83e7a05836472d3586c785ac646
Roger Tuan <r.tuan@datocms.com>
2024-12-09 14:27:22 -0800

    Search improvements

--- src/layouts/docs/BaseLayout/Search/index.tsx
+++ src/layouts/docs/BaseLayout/Search/index.tsx
@@ -1,5 +1,11 @@
 import cn from 'classnames';
-import { useEffect, useState } from 'react';
+import {
+  type ChangeEventHandler,
+  type KeyboardEventHandler,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import { useDebounce } from '@uidotdev/usehooks';
 import s from './style.module.css';
 import type { Result } from './types';
@@ -7,44 +13,116 @@ import { searchInWebsite } from './searchInWebsite';
 import { searchInCommunity } from './searchInCommunity';
 
 import hamburgerIcon from '~/icons/regular/bars.svg?raw';
+import guideIcon from '~/icons/regular/book.svg?raw';
+import forumIcon from '~/icons/regular/comment-alt-lines.svg?raw';
+import pageIcon from '~/icons/regular/file.svg?raw';
+import codeIcon from '~/icons/regular/code.svg?raw';
+import apiIcon from '~/icons/regular/cog.svg?raw';
 
-const areas = [
-  { id: 'general_concepts', label: 'General Concepts' },
-  { id: 'user_guides', label: 'User Guides' },
-  { id: 'academy', label: 'Headless CMS Academy' },
-  { id: 'content_modelling', label: 'Content Modelling' },
-  { id: 'environments', label: 'Environments and Migrations Guide' },
-  { id: 'cda', label: 'Content Delivery API Reference' },
-  { id: 'cma', label: 'Content Management API Reference' },
-  { id: 'plugin_sdk', label: 'Plugin SDK' },
-  { id: 'nuxt', label: 'Nuxt Integration Guide' },
-  { id: 'nextjs', label: 'Next Integration Guide.js' },
-  { id: 'remix', label: 'Remix Integration Guide' },
-  { id: 'sveltekit', label: 'SvelteKit Integration Guide' },
-  { id: 'docs', label: 'Documentation' },
-  { id: 'blog', label: 'Blog' },
-  { id: 'product_updates', label: 'Product Updates' },
-  { id: 'other', label: 'Elsewhere on the website' },
-  { id: 'community', label: 'Community' },
-] as const;
+type Areas = { id: string; label: string; icon?: string; urlMatch?: string }[];
 
-const routeToAreaMatchers: Array<{ match: string; area: (typeof areas)[number]['id'] }> = [
-  { match: '/user-guides', area: 'user_guides' },
-  { match: '/academy', area: 'academy' },
-  { match: '/docs/scripting-migrations', area: 'environments' },
-  { match: '/docs/general-concepts', area: 'general_concepts' },
-  { match: '/docs/content-modelling', area: 'content_modelling' },
-  { match: '/docs/nuxt', area: 'nuxt' },
-  { match: '/docs/next-js', area: 'nextjs' },
-  { match: '/docs/remix', area: 'remix' },
-  { match: '/docs/svelte', area: 'sveltekit' },
-  { match: '/docs/content-delivery-api', area: 'cda' },
-  { match: '/docs/content-management-api', area: 'cma' },
-  { match: '/docs/plugin-sdk', area: 'plugin_sdk' },
-  { match: '/docs', area: 'docs' },
-  { match: '/blog', area: 'blog' },
-  { match: '/product-updates', area: 'product_updates' },
-  { match: 'https://community.datocms.com', area: 'community' },
+const areas: Areas = [
+  {
+    id: 'general_concepts',
+    label: 'General Concepts',
+    icon: guideIcon,
+    urlMatch: '/docs/general-concepts',
+  },
+  {
+    id: 'user_guides',
+    label: 'User Guides',
+    icon: guideIcon,
+    urlMatch: '/user-guides',
+  },
+  {
+    id: 'academy',
+    label: 'Headless CMS Academy',
+    icon: guideIcon,
+    urlMatch: '/academy',
+  },
+  {
+    id: 'content_modelling',
+    label: 'Content Modelling',
+    icon: guideIcon,
+    urlMatch: '/docs/content-modelling',
+  },
+  {
+    id: 'environments',
+    label: 'Environments and Migrations Guide',
+    icon: guideIcon,
+    urlMatch: '/docs/scripting-migrations',
+  },
+  {
+    id: 'cda',
+    label: 'Content Delivery API Reference',
+    icon: apiIcon,
+    urlMatch: '/docs/content-delivery-api',
+  },
+  {
+    id: 'cma',
+    label: 'Content Management API Reference',
+    icon: apiIcon,
+    urlMatch: '/docs/content-management-api',
+  },
+  {
+    id: 'plugin_sdk',
+    label: 'Plugin SDK',
+    icon: apiIcon,
+    urlMatch: '/docs/plugin-sdk',
+  },
+  {
+    id: 'nuxt',
+    label: 'Nuxt Integration Guide',
+    icon: codeIcon,
+    urlMatch: '/docs/nuxt',
+  },
+  {
+    id: 'nextjs',
+    label: 'Next Integration Guide.js',
+    icon: codeIcon,
+    urlMatch: '/docs/next-js',
+  },
+  {
+    id: 'remix',
+    label: 'Remix Integration Guide',
+    icon: codeIcon,
+    urlMatch: '/docs/remix',
+  },
+  {
+    id: 'sveltekit',
+    label: 'SvelteKit Integration Guide',
+    icon: codeIcon,
+    urlMatch: '/docs/svelte',
+  },
+  {
+    id: 'docs',
+    label: 'Developer Docs',
+    icon: codeIcon,
+    urlMatch: '/docs',
+  },
+  {
+    id: 'blog',
+    label: 'Blog',
+    icon: pageIcon,
+    urlMatch: '/blog',
+  },
+  {
+    id: 'product_updates',
+    label: 'Product Updates',
+    icon: pageIcon,
+    urlMatch: '/product-updates',
+  },
+  {
+    id: 'other',
+    label: 'Elsewhere on the website',
+    icon: pageIcon,
+  },
+  {
+    id: 'community',
+    label: 'Community',
+    icon: forumIcon,
+    urlMatch: 'https://community.datocms.com',
+  },
 ];
 
 async function search(query: string) {
@@ -60,8 +138,8 @@ async function search(query: string) {
 
     urls.push(result.url);
 
-    const foundArea = routeToAreaMatchers.find((matcher) => result.url.includes(matcher.match));
-    const area = foundArea ? foundArea.area : 'other';
+    const foundArea = areas.find((area) => area.urlMatch && result.url.includes(area.urlMatch));
+    const area = foundArea ? foundArea.id : 'other';
 
     resultsByArea[area] = resultsByArea[area] || [];
     resultsByArea[area].push(result);
@@ -71,66 +149,100 @@ async function search(query: string) {
 }
 
 export function Search() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchInput, setSearchInput] = useState<string>('');
   const [results, setResults] = useState<Record<string, Result[]>>({});
-  const [isSearching, setIsSearching] = useState(false);
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  const [isLoading, setIsLoading] = useState<boolean>(false); // Waiting for network call?
+  const [isDirty, setIsDirty] = useState<boolean>(false); // Waiting for debounce?
+  const debouncedSearchTerm = useDebounce<string>(searchInput, 500);
+  const hasResults = useMemo<boolean>(() => Object.values(results)?.length > 0, [results]);
+
+  const performSearch = (searchTerm: string) => {
+    setIsLoading(true);
+
+    search(searchTerm).then((results) => {
+      setIsLoading(false);
+      setResults(results);
+    });
+  };
 
   useEffect(() => {
+    setIsDirty(false);
     if (debouncedSearchTerm) {
-      setIsSearching(true);
-
-      search(debouncedSearchTerm).then((results) => {
-        setIsSearching(false);
-        setResults(results);
-      });
+      performSearch(debouncedSearchTerm);
     } else {
       setResults({});
     }
   }, [debouncedSearchTerm]);
 
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key !== 'Enter') {
+      return;
+    }
+    setIsDirty(false);
+    const searchTerm = e.currentTarget.value;
+    if (!searchTerm) {
+      return;
+    }
+
+    setSearchInput(searchTerm);
+    performSearch(searchTerm);
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setIsDirty(true);
+    setSearchInput(e.target.value);
+  };
+
   return (
     <>
-      <div className={cn(s.overlay, searchTerm && s.visible)} onClick={() => setSearchTerm('')} />
-      <div className={cn(s.searchResults, searchTerm && s.visible)}>
-        {isSearching && (
+      <div className={cn(s.overlay, searchInput && s.visible)} onClick={() => setSearchInput('')} />
+      <div className={cn(s.searchResults, searchInput && s.visible)}>
+        {isLoading && (
           <div className={s.spinning}>
             <div />
             <div />
           </div>
         )}
         <div className={s.results}>
-          {areas.map((area) => {
-            if (!results[area.id]) {
-              return null;
-            }
+          {isLoading && <p>Searching for "{searchInput}"...</p>}
 
-            return (
-              <div key={area.id} className={s.area}>
-                <div className={s.areaTitle}>{area.label}</div>
-                {results[area.id]!.map((result) => (
-                  <a href={result.url} key={result.url} className={s.result}>
-                    <div dangerouslySetInnerHTML={{ __html: hamburgerIcon }} />
-                    <div>
-                      <div className={s.resultTitle}>{result.title}</div>
-                      {result.body && <div className={s.resultBody}>{result.body}</div>}
-                    </div>
-                  </a>
-                ))}
-              </div>
-            );
-          })}
+          {!isDirty &&
+            !isLoading &&
+            hasResults &&
+            areas.map((area) => {
+              if (!results[area.id]) {
+                return null;
+              }
+
+              return (
+                <div key={area.id} className={s.area}>
+                  <div className={s.areaTitle}>{area.label}</div>
+                  {results[area.id]!.map((result) => (
+                    <a href={result.url} key={result.url} className={s.result}>
+                      <div dangerouslySetInnerHTML={{ __html: area.icon ?? hamburgerIcon }} />
+                      <div>
+                        <div className={s.resultTitle}>{result.title}</div>
+                        {result.body && <div className={s.resultBody}>{result.body}</div>}
+                      </div>
+                    </a>
+                  ))}
+                </div>
+              );
+            })}
+
+          {!isDirty && !isLoading && !hasResults && (
+            <p>Sorry, no results found for "{searchInput}".</p>
+          )}
         </div>
       </div>
-      <form className={s.formSearch}>
+      <form className={s.formSearch} onSubmit={(e) => e.preventDefault()}>
         <input
           name="query"
           type="search"
           placeholder="Search in the docs and community..."
-          value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
+          value={searchInput}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
         />
       </form>
     </>
