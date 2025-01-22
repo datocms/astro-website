d3ce194a0c03665c91eb342068aee9ea9c2a856d
Stefano Verna <s.verna@datocms.com>
2025-01-22 09:12:38 +0100

    Add GTM

--- src/pages/landing-pages/cookieConsent.js.ts
+++ src/pages/landing-pages/cookieConsent.js.ts
@@ -47,18 +47,24 @@ const gdprCountries = [
   'SK',
 ];
 
-const posthogScript = `
+const scriptsToAddOnCookieConsent = `
 !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t, e) { var o = e.split("."); 2 == o.length && (t = t[o[0]], e = o[1]), t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }(p = t.createElement("script")).type = "text/javascript", p.async = !0, p.src = s.api_host + "/static/array.js", (r = t.getElementsByTagName("script")[0]).parentNode.insertBefore(p, r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-posthog.init('phc_u7FPCuTnUpVlYYTYmfeutKpnI7qEE2cjwgzjz3JDuH6',{api_host:'https://eu.posthog.com'})
+posthog.init('phc_u7FPCuTnUpVlYYTYmfeutKpnI7qEE2cjwgzjz3JDuH6',{api_host:'https://eu.posthog.com'});
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJRM9NT');
 `;
 
 const cookieConsentScript = `
 (() => {
-  const posthogScript = \`${posthogScript}\`;
+  const scriptsToAddOnCookieConsent = \`${scriptsToAddOnCookieConsent}\`;
 
-  function addPosthogScript() {
+  function addScripts() {
     const script = document.createElement('script');
-    script.appendChild(document.createTextNode(posthogScript));
+    script.appendChild(document.createTextNode(scriptsToAddOnCookieConsent));
     document.getElementsByTagName('head')[0].appendChild(script);
   }
 
@@ -177,7 +183,7 @@ const cookieConsentScript = `
 
   onReady(() => {
     if (getCookie(cookieName)) {
-      addPosthogScript();
+      addScripts();
       return;
     }
 
@@ -195,7 +201,7 @@ const cookieConsentScript = `
       .addEventListener('click', () => {
         container.parentNode.removeChild(container);
         setCookie(cookieName, 'true');
-        addPosthogScript();
+        addScripts();
       });
 
     document
@@ -226,7 +232,7 @@ export const GET: APIRoute = async ({ cookies, request }) => {
   return new Response(
     [
       `/* c:${country},a:${consentAccepted ? 'y' : 'n'} */`,
-      showCookieConsent ? cookieConsentScript : posthogScript,
+      showCookieConsent ? cookieConsentScript : scriptsToAddOnCookieConsent,
     ].join('\n'),
     { headers },
   );
