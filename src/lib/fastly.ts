04ed2793ec358cc125840fb7cf076a9459bd7799
Stefano Verna <s.verna@datocms.com>
2025-04-11 16:20:58 +0200

    Split into groups of 256 keys max

--- src/lib/fastly.ts
+++ src/lib/fastly.ts
@@ -1,15 +1,24 @@
 import { FASTLY_KEY, FASTLY_SERVICE_ID } from 'astro:env/server';
 import ky from 'ky';
+import { chunk } from 'lodash-es';
 
 export async function invalidateFastlySurrogateKeys(keys: string[]) {
-  return await ky
-    .post(`https://api.fastly.com/service/${FASTLY_SERVICE_ID}/purge`, {
-      headers: {
-        'fastly-key': FASTLY_KEY,
-        // required for stale-while-revalidate to work
-        'fastly-soft-purge': '1',
-      },
-      json: { surrogate_keys: keys },
-    })
-    .json();
+  const responses = [];
+
+  for (const chunkKeys of chunk(keys, 256)) {
+    const response = await ky
+      .post(`https://api.fastly.com/service/${FASTLY_SERVICE_ID}/purge`, {
+        headers: {
+          'fastly-key': FASTLY_KEY,
+          // required for stale-while-revalidate to work
+          'fastly-soft-purge': '1',
+        },
+        json: { surrogate_keys: chunkKeys },
+      })
+      .json();
+
+    responses.push(response);
+  }
+
+  return responses;
 }
