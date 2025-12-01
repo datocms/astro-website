37ab6278976da6ce71f5bcc3a9e2f410f2e21860
Stefano Verna <s.verna@datocms.com>
2025-12-01 12:58:28 +0100

    Visual Editing

--- src/lib/datocms/executeQuery.ts
+++ src/lib/datocms/executeQuery.ts
@@ -42,6 +42,9 @@ export async function executeQueryOutsideAstro<Result, Variables>(
       excludeInvalid: true,
       includeDrafts: draftModeEnabled,
       token: DATOCMS_API_TOKEN,
+      ...(draftModeEnabled
+        ? { contentLink: 'vercel-v1', baseEditingUrl: 'https://cms.datocms.com' }
+        : {}),
     });
 
     const newCacheTags = datocmsGraphqlResponse.headers.get('x-cache-tags')!.split(' ');
