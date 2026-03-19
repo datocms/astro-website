5a41cef2fa135174c40b1ad631d7494a2d90d9f8
Roger Tuan <r.tuan@datocms.com>
2026-03-19 12:24:41 -0700

    Add inline block support for Github / NPM badge. Also tweaked Github badge label.

--- src/components/blocks/GithubNpmBadge/graphql.ts
+++ src/components/blocks/GithubNpmBadge/graphql.ts
@@ -2,6 +2,8 @@ import { graphql } from '~/lib/datocms/graphql';
 
 export const GithubNpmBadgeFragment = graphql(/* GraphQL */ `
   fragment GithubNpmBadgeFragment on GithubNpmBadgeRecord {
+    id
+    __typename
     githubUrl
     npmUrl
   }
