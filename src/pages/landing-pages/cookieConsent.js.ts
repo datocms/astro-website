a4dfec63e2e0dbae4dd1f2a3990b925b595930e5
Ronak Ganatra <20298819+ronakganatra@users.noreply.github.com>
2026-05-20 11:29:06 +0200

    Remove GTM

--- src/pages/landing-pages/cookieConsent.js.ts
+++ src/pages/landing-pages/cookieConsent.js.ts
@@ -63,11 +63,6 @@ posthog.init('phc_u7FPCuTnUpVlYYTYmfeutKpnI7qEE2cjwgzjz3JDuH6', {
   person_profiles: 'identified_only',
 });
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TJRM9NT');
 `;
 
 const cookieConsentScript = `
